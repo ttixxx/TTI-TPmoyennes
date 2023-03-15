b5016fb29e39a5030dfbf51542110910f8b18b73
Ntran <ntran-hnservices.com>
2023-03-15 14:29:35 +0100

    Update to DotNet7

---
+++ Note.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPMoyennes
+{
+    // Classes fournies par HNI Institut
+    class Note
+    {
+        public int matiere { get; private set; }
+        public float note { get; private set; }
+        public Note(int m, float n)
+        {
+            matiere = m;
+            note = n;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -55,16 +55,6 @@ namespace TPMoyennes
         }
     }
 }
-// Classes fournies par HNI Institut
-class Note
-{
-    public int matiere { get; private set; }
-    public float note { get; private set; }
-    public Note(int m, float n)
-    {
-        matiere = m;
-        note = n;
-    }
-}
+
 
 
